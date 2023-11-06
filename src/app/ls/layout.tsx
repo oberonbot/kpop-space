c7e6d4215f5fea52925d244a92befbab2d631dbe
Hongzhi Zhao (Ethan) <91398474+oberonbot@users.noreply.github.com>
2023-11-06 13:58:56 -0500

    First commit

---
+++ src/app/ls/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './style.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Le Sserafim',
+  description: 'Le Sserafim',
+};
+
+export default function LSLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/tw/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center p-24 gap-32'>
+      <h1 className=' text-8xl'>Twice</h1>
+    </main>
+  );
+};
+
+export default page;
